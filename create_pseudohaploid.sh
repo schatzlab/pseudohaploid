1a3a6b1307641027df70f64c0c6aa23f8afa9819
Mike Schatz <mschatz@cs.jhu.edu>
2018-10-19 08:28:16 -0400

    initial check in

--- create_pseudohaploid.sh
+++ create_pseudohaploid.sh
@@ -1,16 +1,45 @@
 #!/bin/sh
 
+set -o pipefail
+set -xv
+
+BIN="$( cd "$(dirname "$0")" ; pwd -P )"
+
 ## Minimum alignment Identity
-MIN_IDENTITY=.90
+MIN_IDENTITY=90
 
 ## Minimum alignment length to consider
 MIN_LENGTH=1000
 
+## Minimum containment percentage from overlap chains to filter contig
+MIN_CONTAIN=93
+
+if [ $# -lt 2 ]
+then
+  echo "create_pseudohaploid.sh assembly.fa outprefix"
+  exit
+fi
+
 GENOME=$1
+PREFIX=$2
 
-nucmer --maxmatch -c 100 -l 500 $GENOME $GENOME -p self
-delta-filter -l $MIN_LENGTH -i $MIN_IDENTITY self.delta > self.filter.delta
+## You may want to replace this with sge_mummer for large genomes
+## See: https://github.com/fritzsedlazeck/sge_mummer
+nucmer --maxmatch -c 100 -l 500 $GENOME $GENOME -p $PREFIX
 
-$BIN/pseudohaploid.chain.pl self.filter.delta > self.contained
-grep '^#' self.contained > self.contained.id
-$BIN/filter_seq -v self.contained.ids $GENOME > filtered.fa
+## Pre-filter for just longer high identity alignments
+delta-filter -l $MIN_LENGTH -i $MIN_IDENTITY $PREFIX.delta > $PREFIX.filter.delta
+
+## Create the coord file
+show-coords -rclH $PREFIX.filter.delta > $PREFIX.filter.coords
+
+## Find and analyze the alignment chains
+## Note you can rerun this step multiple times from the same coords file
+$BIN/pseudohaploid.chains.pl $PREFIX.filter.coords $MIN_IDENTITY $MIN_CONTAIN > $PREFIX.chains
+
+## Generate a list of contained contigs
+## This can also be rerun from the same chain file but using different containment thresholds
+grep '^#' $PREFIX.chains | awk -v cut=$MIN_CONTAIN '{if ($4 >= cut){print ">"$2}' > $PREFIX.contained.id
+
+## Finally filter the original assembly to remove the contained contigs
+$BIN/filter_seq -v $PREFIX.contained.ids $GENOME > $PREFIX.filtered.fa
