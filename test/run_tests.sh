685d574f3dbc3fe6f725e0195464204b47db9011
Mike Schatz <mschatz@cs.jhu.edu>
2019-02-13 21:49:36 -0500

    add a second test along with expected output

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,5 +1,30 @@
 #!/bin/sh
 
 echo "Running the simple example"
-../create_pseudohaploid.sh simple.fa ph.simple
+echo "==============================================================="
+mkdir -p simple
+cd simple
+ln -sf ../simple.fa
+../../create_pseudohaploid.sh simple.fa ph.simple
+cd ..
 
+echo
+echo "This should report: Pseudohaploid assembly has 1 contigs"
+
+echo "==============================================================="
+
+echo
+echo
+
+
+echo "Running the basic example"
+echo "==============================================================="
+mkdir -p basic
+cd basic
+ln -sf ../basic.fa
+../../create_pseudohaploid.sh basic.fa ph.basic
+cd ..
+
+echo
+echo "This should report: Pseudohaploid assembly has 3 contigs"
+echo "==============================================================="
